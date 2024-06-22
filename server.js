092edb3e55e9ebd693f55ca0ef297216388e1e92
Osiyomeoh <samuelaleonomoh5@gmail.com>
2024-06-22 12:57:35 +0100

    chore: added connection to mongodb atlas and endpoints

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const Contact = require('./models/contact');
+
+const app = express();
+const port = 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://samelect18:CDxf58r3FPCcOT0w@cluster0.7wnpjlr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// Routes
+app.post('/contacts', async (req, res) => {
+  try {
+    const contact = new Contact(req.body);
+    await contact.save();
+    res.status(201).send(contact);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.get('/contacts', async (req, res) => {
+  try {
+    const contacts = await Contact.find();
+    res.status(200).send(contacts);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.put('/contacts/:id', async (req, res) => {
+  try {
+    const contact = await Contact.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    if (!contact) {
+      return res.status(404).send();
+    }
+    res.send(contact);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.delete('/contacts/:id', async (req, res) => {
+  try {
+    const contact = await Contact.findByIdAndDelete(req.params.id);
+    if (!contact) {
+      return res.status(404).send();
+    }
+    res.send(contact);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
